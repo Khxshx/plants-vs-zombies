d6759e90d2082089b6db7a49239aad4045354690
Khxshx <singhkhushi232810@gmail.com>
2021-11-25 00:18:24 +0530

    playername added

--- src/application/homepagecontroller.java
+++ src/application/homepagecontroller.java
@@ -30,6 +30,13 @@ public class homepagecontroller {
 		stage.show();
 	}
 	@FXML
+	public void switchto_homepage(ActionEvent event) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 	public void switchto_username(ActionEvent event) throws IOException{
 		Parent root = FXMLLoader.load(getClass().getResource("/username.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
