4333582a907425f3e00ada82c64e187f22efe17f
Anu Sharma <sharmaanu2002in@gmail.com>
2021-11-25 20:59:07 +0530

    Added zombies

--- src/application/gamepagecontroller.java
+++ src/application/gamepagecontroller.java
@@ -1,13 +1,30 @@
 package application;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class gamepagecontroller {
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class gamepagecontroller implements Initializable{
 	@FXML
 	Label playername;
-	
+	@FXML
+	private ImageView zombieImage;
 	public void displayName(String username) {
 		playername.setText("PLAYER : " + username);
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		TranslateTransition translate = new TranslateTransition();
+		translate.setNode(zombieImage);
+		translate.setDuration(Duration.millis(20000));
+		translate.setByX(-500);
+		translate.play();
 	}
 }
